fa56f995dfb74a8493e55dbdcfb100eee34e902e
Joe Collins <joejcollins@gmail.com>
2014-12-17 12:17:33 +0000

    Changed the Google Analytics code to auto, to see if it will help.

--- web_app/application/views/layout.php
+++ web_app/application/views/layout.php
@@ -161,13 +161,14 @@ body { padding-top: 60px;/* 60px to make the container go all the way to the bot
 <?php endforeach; } ?>
 <script type="text/javascript">
     if (jQuery.cookie('cc_cookie_accept') == "cc_cookie_accept") {
-    	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-    		  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-    		  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-    		  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-    		  ga('create', 'UA-36961584-3', 'bosworthbattlefield.com');
-    		  ga('send', 'pageview');
+		ga('create', 'UA-36961584-3', 'auto');
+		ga('send', 'pageview');
+
 		// Update the news
       	$(".tweet").tweet({
       	  username: "BosworthLCC",
